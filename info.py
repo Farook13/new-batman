7ab0483bb15adf38548eab4421943fd8b23cf375
Farook13 <umarulfarook787@gmail.com>
2025-05-19 14:46:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,11 +70,11 @@ else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://pcmovies:pcmovies@cluster0.4vv9ebl.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://batman13:batman13@batman.sawvl.mongodb.net/?retryWrites=true&w=majority&appName=batman")
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
-DATABASE_NAME = environ.get('DATABASE_NAME', "pcmovies")
+DATABASE_NAME = environ.get('DATABASE_NAME', "batman13")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
